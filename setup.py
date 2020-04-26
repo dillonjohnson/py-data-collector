dd95c738dec5cf19c7eb0f11f1be4f07275773bc
Dillon Johnson <djrepec@gmail.com>
2020-04-26 14:28:31 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='py-data-collector',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='dillonjohnson',
+    author_email='dillonjohnson1015@gmail.com',
+    description='This library is to collect various data points.'
+)
